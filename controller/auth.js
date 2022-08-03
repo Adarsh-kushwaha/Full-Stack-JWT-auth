665845ec3986a1d175e4be4702653abf1c51d061
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-08-03 15:43:44 +0530

    added login functionality

--- controller/auth.js
+++ controller/auth.js
@@ -21,8 +21,47 @@ const register = async (req, res, next) => {
     }
 }
 
-const login = (req, res, next) => {
-    res.send("login route")
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400).json({
+            success: false,
+            error: "please provide email and password"
+        })
+    }
+
+    try {
+
+        const user = await dbuser.findOne({ email }).select("password");
+
+        if (!user) {
+            res.status(404).json({
+                success: false,
+                error: "user not found"
+            })
+        }
+
+        const isMatch = await user.matchPasswords(password);
+
+        if (!isMatch) {
+            res.status(404).json({
+                success: false,
+                error: "invallid credential"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            token: "kok7\8\57dco"
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message
+        })
+    }
 }
 
 const forgetPassword = (req, res, next) => {
